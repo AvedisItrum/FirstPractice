3287aca951a0824a291b7080b8cca72eca0c8abb
AvedisItrum <avedis.itrum@gmail.com>
2023-05-22 11:32:12 +0300

    CustomException updated

--- src/main/java/com/example/apiswagger/domain/web/dto/CustomException.java
+++ src/main/java/com/example/apiswagger/domain/web/dto/CustomException.java
@@ -1,7 +1,16 @@
 package com.example.apiswagger.domain.web.dto;
 
-public class CustomException extends RuntimeException{
+import jakarta.persistence.EntityNotFoundException;
+
+import java.lang.reflect.Type;
+import java.util.function.Supplier;
+
+public class CustomException extends RuntimeException {
     public CustomException(String message) {
         super(message);
+    }
+
+    public static Supplier<EntityNotFoundException> IdNotFound(Type type) {
+        return () -> new EntityNotFoundException(type.getTypeName() + " with ID \" + id + \" not found");
     }
 }
